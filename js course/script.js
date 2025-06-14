65527ac79a9f1a3664b7043b6c97007ce35ea0e7
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-06-13 22:36:33 -0500

    commit

--- js course/script.js
+++ js course/script.js
@@ -116,16 +116,48 @@ console.log(century); */
 //------------------------------------------------------
 // type conversion and coercion
 //------------------------------------------------------
-const number = Number("2");
+// type conversion
+/* const number = Number("2");
 console.log(typeof number);
 
 const inputYear = '1991';
 console.log(`Number: ${Number(inputYear)} | String: "${inputYear}"`);
 console.log(inputYear + 18);
-// greet('Hi')('Isaac');
+
+console.log(Number('Isaac'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+
+//type coercion
+console.log('I am ' + 23 + ' years old.');
+console.log('I am ' + '23' + ' years old'); */
 
 
 
+//------------------------------------------------------
+// truthy and falsy
+//------------------------------------------------------
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Isaac'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if(money){
+    console.log("Don't spend it all.");
+} else{
+    console.log("You should get a job")
+}
+
+let height = 123;
+if(height){
+    console.log('Yay! height is defined.');
+} else{
+    console.log('Height is UNDEFINED');
+}
 
 
 //========================================================================================================================
