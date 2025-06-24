18e1d5be500ec31ee8579fe83ba0ee87615ced05
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-06-24 17:27:49 -0500

    COMMIT

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>hello</p>
-    </div>
-  );
+	return (
+		<div>
+			<p>hello</p>
+		</div>
+	);
 }
 
 export default App;
