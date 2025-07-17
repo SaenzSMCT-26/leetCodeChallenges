aeb7b910329ab49fef79b547a06b8f486285a69c
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-07-17 18:17:29 -0500

    commit

--- Data Structures and Algorithms/algo challenges/sorting algorithms/selection_sort.js
+++ Data Structures and Algorithms/algo challenges/sorting algorithms/selection_sort.js
@@ -7,7 +7,6 @@ when the loops run the next time around, min
 will always reset to the next index value in 
 the iteration.
 */
-		// 	    		    [2, 1, 5, 4, 3]
 		let min = i; // index# 0, 1, 2, 3, 4
 		for (let j = i + 1; j < arr.length; j++) {
 			/* 
@@ -28,7 +27,7 @@ inner loop, j and min will update accordingly.
 	return arr;
 }
 
-const array1 = [2, 1, 5, 4, 3];
+const array1 = [22, 11, 31, 54, 42];
 
 console.log(selectionSort(array1));
 
@@ -38,6 +37,10 @@ our array called array_to_loop [2, 1, 3, 5, 4];
 arr is the parameter, which is representative of the input we 
 will use for the function. In this case, array_to_loop.
 
+
+notes: min always updates when the loop iteration is running, but when i moves on to the
+next iteration it always resets to the next index value i represents because min is decalred
+inside the outerloop. without trying to sound like a broken record, "min" resets to the next iteration value of the outer loop variable "i" 
 ==============================================
 1st iteration:
 ==============================================
@@ -64,8 +67,8 @@ arr[j] now equals 42.
 
 
 arr[min]|11| < arr[j]|42|:
-arr[min]|11| is less than arr[j]|42|, so arr[min] stays at 11. iteration 
-of inner loop stops and arr[min]|11| swaps with arr[i]|22|.
+arr[min]|11| is less than arr[j]|42|, so arr[min] stays at 11. iteration
+of inner loop stops and arr[min]|11|[index 1] swaps with arr[i]|22|[index 0].
 
 
 
@@ -75,7 +78,7 @@ of inner loop stops and arr[min]|11| swaps with arr[i]|22|.
 ==============================================
 array_to_loop: [11, 22, 31, 54, 42]
 min = i: [index value 1]|22|
-min = i + 1: [index value 2]|31|
+j = i + 1: [index value 2]|31|
 
 arr[min] starts at 22 and arr[j] starts at 31.
 
@@ -101,5 +104,31 @@ of inner loop stops and arr[min]|22| doesn't swap.
 ==============================================
 array_to_loop: [11, 22, 31, 54, 42]
 min = i: [index value 2]|31|
-min = i + 1: [index value 3]|54|
+j = i + 1: [index value 3]|54|
+
+arr[min] starts at 31 and arr[j] starts at 54.
+
+arr[min]|31| < arr[j]|54|:
+arr[min]|31| is less arr[j]|54|, so arr[min] stays at 31 and
+arr[j] now equals 42.
+
+arr[min]|31| < arr[j]|42|:
+arr[min]|31| is less than arr[j]|42|, so arr[min] stays at 31. itertion
+of inner loop stops and arr[min]|31| doesn't swap.
+
+
+
+
+==============================================
+4th iteration:
+==============================================
+array_to_loop: [11, 22, 31, 54, 42]
+min = i: [index value 2]|54|
+j = i + 1: [index value 3]|42|
+
+arr[min] stars at 54 and arr[j] starts at 42.
+
+arr[min]|54| > arr[j]|42|:
+arr[min]|54| is greater than arr[j]|42|, so arr[min] now equals 42. iteration
+of the inner loop stops and arr[min]|42|[index 4] swaps with arr[i]|54|[index 3]
 */
